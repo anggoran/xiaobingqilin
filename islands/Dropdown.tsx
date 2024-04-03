5c85e1e25776708eff7d75720080b54316ba7a27
Anggoran <anggoranm@gmail.com>
2024-04-03 16:18:11 +0700

    feature/dropdown-state

--- islands/Dropdown.tsx
+++ islands/Dropdown.tsx
@@ -1,15 +1,21 @@
+import { Signal } from "@preact/signals";
+import { JSX } from "preact/jsx-runtime";
+
 interface DropdownProps {
   section: string;
-  data: { id: number; name: string }[];
+  data: { id: string; name: number }[] | undefined;
+  state: Signal<string>;
 }
 
 export function Dropdown({ props }: { props: DropdownProps }) {
+  const handleChange = (e: JSX.TargetedEvent) => {
+    props.state.value = (e.target as HTMLInputElement).value;
+  };
+
   return (
-    <select name={props.section}>
+    <select name={props.section} onChange={handleChange}>
       <option value="" hidden disabled selected>{props.section}</option>
-      {props.data.map((e) => {
-        return <option value={e.id}>{e.name}</option>;
-      })}
+      {props?.data?.map((e) => <option value={e.id}>{e.name}</option>)}
     </select>
   );
 }
--- islands/Menu.tsx
+++ islands/Menu.tsx
@@ -1,16 +1,28 @@
+import { JSX } from "preact/jsx-runtime";
+import { Signal } from "@preact/signals";
+
 interface MenuProps {
   section: string;
-  data: { id: number; name: string }[];
+  data: { id: string; name: number }[] | undefined;
+  state: Signal<string>;
 }
 
 export function Menu({ props }: { props: MenuProps }) {
+  const handleChange = (e: JSX.TargetedEvent) => {
+    props.state.value = (e.target as HTMLInputElement).value;
+  };
+
   return (
     <>
-      <input list={props.section} placeholder={props.section} />
-      <datalist id={props.section}>
-        {props.data.map((e) => {
-          return <option value={e.name} />;
-        })}
+      <input
+        placeholder={props.section}
+        list={props.section}
+        onChange={handleChange}
+      />
+      <datalist
+        id={props.section}
+      >
+        {props?.data?.map((e) => <option value={e.name} />)}
       </datalist>
     </>
   );
