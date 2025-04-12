30e86773b3d363276cf3342639c2707e749ab212
Anggoran <anggoranm@gmail.com>
2025-04-13 00:20:45 +0700

    add: magic link auth

---
+++ routes/api/auth/magic/send.ts
@@ -0,0 +1,67 @@
+import { Handlers } from "$fresh/server.ts";
+import { Surreal } from "@surrealdb/surrealdb";
+import nodemailer from "npm:nodemailer@6.10.0";
+import { AuthServiceImpl } from "../../../../services/auth.ts";
+import { generateFisherToken } from "../../../../utils/fisher-token.ts";
+import { encodeBase64Url } from "@std/encoding";
+import { VerifierRequest } from "../../../../models/auth.ts";
+import { ENV } from "../../../../utils/config.ts";
+
+export const handler: Handlers = {
+	async POST(req) {
+		const headers = new Headers(req.headers);
+		const body = await req.json();
+
+		const { value, error } = VerifierRequest.validate(body);
+		if (error) return new Response("Invalid request.", { status: 400 });
+		const { accessType, email, device: { mac, ip } } = value;
+
+		const db = new Surreal();
+		const auth = new AuthServiceImpl(db);
+
+		let challenge: string;
+
+		try {
+			await db.connect(ENV.SURREAL_DB_URL);
+			await auth.accessSystem();
+
+			const limit = await auth.countVerifier(email, { mac, ip });
+			if (limit && limit >= 5) {
+				return new Response("Request exceeded.", { status: 429 });
+			}
+
+			challenge = generateFisherToken(email);
+			const base64url_token = encodeBase64Url(challenge);
+
+			// Determine the base URL based on the destination header
+			const destination = headers.get("destination");
+			const protocol = destination?.startsWith("localhost") ? "http" : "https";
+			const baseURL = `${protocol}://${destination}`;
+
+			// Read and customize email template
+			const content = await Deno.readTextFile("./static/email.html");
+			const link = `${baseURL}/auth/${accessType}?verify=${base64url_token}`;
+			const html = content.replace("{{link_to_app}}", link);
+
+			const transporter = nodemailer.createTransport({
+				host: ENV.SMTP_HOST,
+				port: ENV.SMTP_PORT,
+				secure: ENV.SMTP_SECURITY,
+				auth: ENV.SMTP_AUTH,
+			});
+
+			await transporter.sendMail({
+				from: ENV.SMTP_SENDER,
+				to: [email],
+				subject: "Please continue your login!",
+				html,
+			});
+		} catch (e) {
+			return new Response("Email error: " + e, { status: 500 });
+		} finally {
+			await db.close();
+		}
+
+		return new Response(challenge, { status: 201 });
+	},
+};
