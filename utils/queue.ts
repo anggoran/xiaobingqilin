e736f2400f8725e1c5f21075e6c733985a7bc08c
Anggoran <anggoranm@gmail.com>
2025-04-19 19:17:17 +0700

    modify: refactor queue code

--- utils/queue.ts
+++ utils/queue.ts
@@ -1,2 +1,19 @@
 /// <reference lib="deno.unstable" />
+
+import { sendSignInMail } from "../controllers/async/signin.ts";
+import { sendSignUpMail } from "../controllers/async/signup.ts";
+
 export const denokv = await Deno.openKv();
+
+export const backgroundTask = () => {
+	return denokv.listenQueue(async (msg) => {
+		switch (msg.type) {
+			case "send-signup-mail":
+				await sendSignUpMail(msg.value);
+				break;
+			case "send-signin-mail":
+				await sendSignInMail(msg.value);
+				break;
+		}
+	});
+};
