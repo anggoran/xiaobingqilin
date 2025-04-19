d96c824701b58594122d71ff4309241c25908b3f
Anggoran <anggoranm@gmail.com>
2025-04-19 19:17:03 +0700

    modify: include tailwind in build

--- dev.ts
+++ dev.ts
@@ -1,5 +1,9 @@
 #!/usr/bin/env -S deno run -A --watch=static/,routes/
 
 import dev from "$fresh/dev.ts";
+import config from "./fresh.config.ts";
+import { backgroundTask } from "./utils/queue.ts";
 
-await dev(import.meta.url, "./main.ts");
+if (!Deno.args.includes("build")) backgroundTask();
+
+await dev(import.meta.url, "./main.ts", config);
